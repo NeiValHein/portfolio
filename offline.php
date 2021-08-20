c0534cbb18f7bd29c111fffdcc3f0159f6348f54
NeiValHein <37918247+NeiValHein@users.noreply.github.com>
2021-08-20 15:02:41 -0600

    Update offline.php

--- offline.php
+++ offline.php
@@ -4,54 +4,35 @@
 <?php include "layout/head.php"; ?>
 <title>Ups!</title>
 <meta http-equiv="X-UA-Compatible"content="IE=Edge">
-<meta name="description" content="Error De Conexión.">
-<meta name="author" content="WebCodeTeam">
-<style>
-body {text-align: center;}
-.content { position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%) }
-</style>
+<meta name="description" content="Connection Error.">
+<meta name="author" content="NeiValHein">
 </head>
 <body>
-<div class="content">
-<div class="offline" style="height: 300px;"></div>
-<div class="text">😬 Estas sin conexión, pero puedes seguir leyendo los posts que guardaste. 🥳</div>
+<div class="content-container text-center">
+<div class="offline-error" style="height: 300px;"></div>
+<div>Hey, you're currently offline &#10071;</div>
 <span class="brsmall"></span>
-<a class="btn" onclick="queryCache()">Obtener Posts</a>
-<div id="cached_posts"></div>
+<div class="conection-check" id="conection-check"><p>Current connection status: <span id="status"></span></p>
+<button class="btn refresh" onclick="refresh()">Reload</button></div>
 </div>
 <script type="text/javascript">
-var run = false;
-function queryCache(){
-    if(run) {
-       return;
-    } 
-    run = true;
-    var url = [];
-    caches.open('wct-posts').then(function (cache){
-        cache.keys().then(function(keys){
-            return Promise.all(
-                    keys.map(function(k){url.push(k.url); return k.url} )
-                )
-        }).then(function(u){ cacheList(url);})
-    })
-}
-function cacheList(Items)
-{   
-div = document.createElement('div');
-document.getElementById('cached_posts').appendChild(div);
-if (Items.length >= 1) {
-for(var i = 0; i < Items.length; i++){
-        console.log('Cacheado: '+Items[i]);    
-    let a = document.createElement('a');
-    div.appendChild(a);
-    a.innerHTML = '<a href="'+Items+'">' +Items+'</a>'+'<br>';
-    }
-}
-else {
-    let p = document.createElement('p');
-    div.appendChild(p);
-    p.innerHTML = 'Ups!, parece que todavia no has guardado ningun post :/';    
-}
-};</script>
+var loaded = false;
+window.addEventListener('load', function() {
+  var status = document.getElementById("status");
+
+  function updateOnlineStatus(event) {
+    var condition = navigator.onLine ? "online" : "offline";
+
+    status.className = condition;
+    status.innerHTML = condition.toUpperCase();
+  }
+  window.addEventListener('online',  updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+});
+function refresh(){
+    document.querySelector('.conection-check').innerHTML = 'Re-establishing connection please wait...' +'<div class="loader"></div>';
+    window.location.reload();
+} 
+</script>
 </body>
 </html>
